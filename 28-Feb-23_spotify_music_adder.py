54245b12734f4a962493f0b21b0c93cf6d78e54b
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-07-27 13:28:47 +0530

    Update 28-Feb-23_spotify_music_adder.py

--- 28-Feb-23_spotify_music_adder.py
+++ 28-Feb-23_spotify_music_adder.py
@@ -28,6 +28,7 @@ read_token = util.prompt_for_user_token(username, "user-library-read")
 read_scoped_sp = spotipy.Spotify(auth=read_token)
 
 write_token = util.prompt_for_user_token(username, "user-library-modify")
+
 write_scoped_sp = spotipy.Spotify(auth=write_token)
 
 def reduce_track_information(track_list):
