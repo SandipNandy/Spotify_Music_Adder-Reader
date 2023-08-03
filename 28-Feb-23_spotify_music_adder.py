7c23e9dd1b054d85d2417f6cf6d38432318c36e2
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-03 12:05:40 +0530

    Update 28-Feb-23_spotify_music_adder.py

--- 28-Feb-23_spotify_music_adder.py
+++ 28-Feb-23_spotify_music_adder.py
@@ -14,6 +14,7 @@ We need to export the following environemt variables in our Terminal
 
 # create the spotipy instance with the read and write scope
 # set a fixed username for now
+
 username = "aowve1162mwuc9f56s3k3gxsu"
 read_token = util.prompt_for_user_token(username, "user-library-read")
 read_scoped_sp = spotipy.Spotify(auth=read_token)
