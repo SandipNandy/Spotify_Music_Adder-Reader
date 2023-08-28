4c1c6949d9c1824836368afd6d8ceee7f2c4521c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-08-28 13:32:57 +0530

    Update 28-Feb-23_spotify_music_adder_read.js

--- 28-Feb-23_spotify_music_adder_read.js
+++ 28-Feb-23_spotify_music_adder_read.js
@@ -11,7 +11,6 @@ const prompt = require('prompt-sync')();
 const express = require('express');
 const cors = require('cors');
 const open = require('open');
-
 const app = express();
 
 app.use(cors());
