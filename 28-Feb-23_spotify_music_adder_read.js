92a36e7dca2734811e0591509034a82b6f4a9e35
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-10 09:41:48 +0530

    Update 28-Feb-23_spotify_music_adder_read.js

--- 28-Feb-23_spotify_music_adder_read.js
+++ 28-Feb-23_spotify_music_adder_read.js
@@ -14,7 +14,6 @@ const express = require('express');
 const cors = require('cors');
 const open = require('open');
 const app = express();
-
 app.use(cors());
 let server = null;
 const clientId = 'ENTER CLIENT ID HERE';
